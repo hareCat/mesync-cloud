8507eed16da99ebbaeec51161e8cd656f7d14ef6
iplion <lionevseev@gmail.com>
2026-01-24 13:21:48 +0700

    Initial commit

---
+++ src/main/java/com/iplion/mesync/cloud/MesyncCloudApplication.java
@@ -0,0 +1,13 @@
+package com.iplion.mesync.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesyncCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MesyncCloudApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/iplion/mesync/cloud/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.iplion.mesync.cloud.config;
+
+public class SecurityConfig {
+}
